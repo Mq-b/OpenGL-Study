0e42ea108abcaba20ab40a5bce52439ebee646f2
mq白 <3326284481@qq.com>
2026-01-14 11:13:12 +0800

    新增 compileShader 模板函数并激活着色器程序

--- examples/DrawTriangle.cpp
+++ examples/DrawTriangle.cpp
@@ -36,12 +36,68 @@ GLFWwindow* createWindow(std::size_t width, std::size_t height, const char* titl
     return window;
 }
 
+template<typename T>
+concept as_char_ptr = std::convertible_to<T, const char*>;
+
+/**
+ * @brief 编译并链接多个着色器程序
+ * 
+ * @tparam types 
+ * @param ShaderSources 
+ * @return unsigned int 
+ */
+template<std::size_t... types>
+unsigned int compileShader(const as_char_ptr auto&... ShaderSources) {
+    auto compileIndividualShader = [](const char* source,int type) {
+        unsigned int shader = glCreateShader(type);
+        glShaderSource(shader, 1, &source, NULL);
+        glCompileShader(shader);
+        return shader;
+    };
+    //解包参数并编译每个着色器
+    std::array array = { compileIndividualShader(ShaderSources,types)... };
+    // 链接
+    unsigned int program = glCreateProgram();
+    for(const auto& shader : array)
+        glAttachShader(program, shader);
+
+    glLinkProgram(program);
+    // 释放
+    for(const auto& shader : array)
+        glDeleteShader(shader);
+
+    return program;
+}
+
+// 顶点着色器源码
+const char *vertexShaderSource = R"(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    void main()
+    {
+       gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+)";
+
+// 片段着色器源码：决定最终像素的颜色
+const char *fragmentShaderSource = R"(
+    #version 330 core
+    out vec4 FragColor;
+    void main()
+    {
+       FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); // 橘黄色 (RGBA)
+    }
+)";
+
 int main(){
     // 初始化 glfw 使用 OpenGL3.3 核心模式
     initGLFW();
     // 创建窗口，设置OpenGL上下文，并加载 GLAD 激活 OpenGL 的函数指针。
     auto window = createWindow(800, 600, "OpenGL Triangle");
-    
+
+    // 编译链接着色器程序
+    auto shaderProgram = compileShader<GL_VERTEX_SHADER, GL_FRAGMENT_SHADER>(vertexShaderSource, fragmentShaderSource);
+
     // 三角形顶点数据
     std::array vertices {
         0.0f,  0.5f, 0.0f,  // 顶点1 (顶部)
@@ -66,6 +122,9 @@ int main(){
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // 设置清屏颜色，四个参数代表 RGBA
         glClear(GL_COLOR_BUFFER_BIT);         // 执行清屏操作
 
+        // 激活着色器程序
+        glUseProgram(shaderProgram);
+
         // 重复绑定 顶点数组对象（只绘制这一个图形，可有可无）
         glBindVertexArray(VAO);
         // 绘图指令：通知 GPU 按照“三角形”规则，处理当前绑定的 VAO 里的前 3 个顶点数据（每抓到 3 个点，就自动把它们首尾相连，中间涂满颜色，形成一个实心三角形），在后台绘制
@@ -77,6 +136,8 @@ int main(){
         // GLFW 处理窗口事件，比如键盘鼠标输入等
         glfwPollEvents();
     }
+    // 清理着色器程序资源
+    glDeleteProgram(shaderProgram);
     // 释放 GLFW 资源
     glfwTerminate();
 }
